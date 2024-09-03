971a7ae9763c1c1cc181d9c588f868d6bd2d642c
rinesagoxhufi <rinesagoxhufii@gmail.com>
2024-09-03 04:50:42 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as SplashScreen from 'expo-splash-screen';
+
+
+import AllPlaces from './screens/AllPlaces';
+import AddPlace from './screens/AddPlace';
+import IconButton from './components/UI/IconButton';
+import { Colors } from './constants/colors';
+import Map from './screens/Map';
+import { init } from './util/database';
+import PlaceDetails from './screens/PlaceDetails';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync(); // Prevent auto-hide of splash screen
+        await init(); // Initialize your database
+        setDbInitialized(true);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        await SplashScreen.hideAsync(); // Hide the splash screen
+      }
+    };
+    prepare();
+  }, []);
+
+  if (!dbInitialized) {
+    return null; // Optionally return a fallback UI while loading
+  }
+
+  return (
+    <>
+      <StatusBar style='dark' />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+          headerStyle: { backgroundColor: Colors.primary500},
+          headerTintColor: Colors.gray700,
+          contentStyle: {backgroundColor: Colors.gray700},
+        }} >
+          <Stack.Screen name="AllPlaces" component={AllPlaces} options={({ navigation }) => ({
+            title: 'Your favorite Places',
+            headerRight: ({ tintColor }) => (
+              <IconButton
+                icon="add" 
+                size={24} 
+                color={tintColor} 
+                onPress={() => navigation.navigate('AddPlace')} 
+              />
+            ),
+          })} />
+          <Stack.Screen name="AddPlace" component={AddPlace} options={{
+            title: 'Add a new Place'
+          }}/>
+          <Stack.Screen name="Map" component={Map} />
+          <Stack.Screen name="PlaceDetails" component={PlaceDetails} options={{
+            title: 'Loading Place...',
+          }}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
---
+++ screens/AllPlaces.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import PlacesList from "../components/Places/PlacesList";
+import { useIsFocused } from "@react-navigation/native";
+import { fetchPlaces } from "../util/database";
+
+function AllPlaces({ route }) {
+    const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+    const isFocused = useIsFocused();
+
+
+    useEffect(() => {
+        async function loadedPlaces() {
+        const places =   await  fetchPlaces();     
+        setLoadedPlaces(places);    
+        }
+        if (isFocused) {
+            loadedPlaces();
+  //          setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+        }
+    }, [isFocused]);
+
+    return <PlacesList places={loadedPlaces} />;
+}
+
+export default AllPlaces;
